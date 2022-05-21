d9eb17220a53e569d44685183b2c74e923d14ecd
arihantkumar01 <arihantkumar0101@gmail.com>
2022-05-21 13:40:57 +0530

    User details can now registered using data binding, and api call

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
@@ -14,6 +15,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { LoginComponent } from './pages/login/login.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -30,7 +32,9 @@ import { LoginComponent } from './pages/login/login.component';
     MatButtonModule,
     MatIconModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService:UserService) { }
+
+  public user={
+    username: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    mobile: '',
+  }
 
   ngOnInit(): void {
+  }
+
+  formSubmit()
+  {
+    this.userService.addUser(this.user).subscribe(
+      (data)=>{
+        // success
+        console.log(data)
+        alert("success")
+      },
+      (error)=>{
+        // error
+        console.log(error)
+        alert("error")
+      }
+    )
   }
 
 }
